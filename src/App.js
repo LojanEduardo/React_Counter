ee09206c1caf87df79a9beb6bccb4c19293bc225
Lojan Eduardo <lojanpetrovich@gmail.com>
2021-12-21 23:42:51 -0300

    Estudo React, usando useState

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  //count
+  const [count, setCount] = useState(0);
+  const [colorMinus, setColorMinus] = useState("#adda1a");
+  const [colorPlus, setColorPlus] = useState("#3e8e41");
+
+  const handleMinusClick = () => {
+    if(count > 0){
+      setCount(count - 1);
+    }if(count == 1 || count == 0){
+      setColorMinus("red");
+    }
+    setColorPlus("#3e8e41");
+  }
+
+  const handlePlusClick = () => {
+    if(count < 10){
+      setCount(count + 1)
+      if(count == 9){
+        setColorPlus("red");
+      }
+      setColorMinus("#adda1a");   
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Contador: {count}</h1>
+
+      <button 
+      onClick={handlePlusClick} style={{backgroundColor: colorPlus}} className="mais">+</button>
+
+      <button onClick={handleMinusClick} style={{backgroundColor: colorMinus}} className="menos">-</button>
+    </>
   );
 }
 
